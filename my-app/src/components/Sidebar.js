7dc55fe626ab3ab1cc0027ed5d48165104e142eb
Sam Green <sam.green7777@yahoo.com>
2024-11-02 14:15:01 +0000

    add code to sidebar

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Sidebar(props) {
+  return (
+    <div>
+      <a href="#." target="_blank">
+        My Photos
+      </a>
+      <a href="#." target="_blank">
+        My Illustrations
+      </a>
+      <a href="#." target="_blank">
+        My Paintings
+      </a>
+    </div>
+  );
+}
