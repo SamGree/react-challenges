a6ada82e6f5b92019b6baf4469c4c09da62d596d
Sam Green <sam.green7777@yahoo.com>
2024-11-03 16:32:08 +0000

    add new and alot

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import css from './App.module.css';
+import NavBarSimple from "./components/NavBarSimple";
 import Sidebar from "./components/Sidebar";
+import StatefulGreeting from "./components/StatefulGreeting";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
-      <Sidebar />
+      <NavBarSimple />
+      <Sidebar /> 
+      <div className="App">
+    <StatefulGreeting name="Mike"/>
+  </div>
+
     </div>
+    
   );
 }
 
